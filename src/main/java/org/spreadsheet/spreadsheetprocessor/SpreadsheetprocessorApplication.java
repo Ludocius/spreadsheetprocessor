d983545b83ab60e6952265ea5af129d9630716f3
Luis Fernando Pinzon Marroquin <59232095+Ludocius@users.noreply.github.com>
2024-01-13 17:49:02 -0500

    :construction: initial project

---
+++ src/main/java/org/spreadsheet/spreadsheetprocessor/SpreadsheetprocessorApplication.java
@@ -0,0 +1,13 @@
+package org.spreadsheet.spreadsheetprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadsheetprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadsheetprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spreadsheet/spreadsheetprocessor/SpreadsheetprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spreadsheet.spreadsheetprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadsheetprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
